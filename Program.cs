1d87d8b4b3df677c119e2e9c64f13d0af077e948
morccc <marccapucion235@gmail.com>
2026-05-13 15:04:35 +0800

    Program.cs

--- Program.cs
+++ Program.cs
@@ -2,13 +2,42 @@
 
 class Product
 {
-    public int Id;
-    public string Name;
-    public string Category;
-    public double Price;
-    public int RemainingStock;
+    private int id;
+    private string name;
+    private string category;
+    private double price;
+    private int remainingStock;
+
+    public void SetId(int id) { this.id = id; }
+    public void SetName(string name) { this.name = name; }
+    public void SetCategory(string category) { this.category = category; }
+    public void SetPrice(double price) { this.price = price; }
+    public void SetRemainingStock(int stock) { this.remainingStock = stock; }
+
+     public int GetId() { return id; }
+    public string GetName() { return name; }
+    public string GetCategory() { return category; }
+    public double GetPrice() { return price; }
+    public int GetRemainingStock() { return remainingStock; }
+
+    public void ReduceStock(int qty)
+    {
+        remainingStock -= qty;
+    }
 
     public void DisplayProduct()
+    {
+        Console.WriteLine($"{id}. {name} ({category}) - P{price} (Stock: {remainingStock})");
+    }
+}
+
+
+
+
+
+
+
+    
     {
         Console.WriteLine($"{Id}. {Name} ({Category}) - P{Price} (Stock: {RemainingStock})");
     }
